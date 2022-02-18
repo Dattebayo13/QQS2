6a5919b5ca874a5b49673c50f8551f7f30e79367
Dattebayo13 <anonymous.2003.69@gmail.com>
2022-02-18 21:55:05 +0530

    Added 2 episodes

---
+++ build.gradle.kts
@@ -0,0 +1,122 @@
+import java.time.*
+import myaa.subkt.ass.*
+import myaa.subkt.tasks.*
+import myaa.subkt.tasks.Mux.*
+
+plugins { id("myaa.subkt") }
+
+subs {
+    readProperties("sub.properties")
+    episodes(getList("episodes"))
+
+    
+	merge {
+	    onStyleConflict(ErrorMode.FAIL)
+
+        from(get("dialogue")) {
+		    incrementLayer(10)
+		}
+
+        if (propertyExists("OP")) {
+            from(get("OP")) { syncTargetTime(getAs<Duration>("opsync")) }
+        }
+
+        if (propertyExists("ED")) {
+            from(get("ED")) { syncTargetTime(getAs<Duration>("edsync")) }
+        }
+
+        if (file(get("typesetting")).exists()) {
+            from(get("typesetting"))
+        }
+		out(get("mergedname"))
+    }
+	
+    val merge_ss by task<Merge> {
+        if (propertyExists("OP")) {
+            from(get("OP")) { syncTargetTime(getAs<Duration>("opsync")) }
+        }
+
+        if (propertyExists("ED")) {
+            from(get("ED")) { syncTargetTime(getAs<Duration>("edsync")) }
+        }
+
+        if (file(get("typesetting")).exists()) {
+            from(get("typesetting"))
+        }
+		out(get("mergednamess"))
+        }
+		
+	chapters {
+        from(get("dialogue"))
+        chapterMarker("chptr")
+    }
+
+
+    mux {
+        title(get("title"))
+
+        from(get("premux")) {
+            video {
+			    name("[Vardë]")
+                lang("jpn")
+                default(true)
+            }
+            audio { include(false) }
+			includeChapters(false)
+            attachments { include(false) }
+        }
+
+		from(get("jpnaudio")) {
+            audio {
+                name("")
+				lang("jpn")
+                default(true)
+        }}
+        		
+		from(get("dub")) {
+            audio {
+                name("")
+				lang("eng")
+                default(false)
+        }}
+        
+		from(merge.item()) {
+            tracks {
+                lang("eng")
+                name(get("subtitle_full"))
+                default(true)
+                forced(false)
+				
+            }
+        }
+
+        from(merge_ss.item()) {
+            tracks {
+                lang("eng")
+                name(get("subtitle_ss"))
+                default(false)
+                forced(true)
+				
+            }
+        }
+		chapters(chapters.item()) { lang("eng") }
+
+		attach(get("common_fonts")) { includeExtensions("ttf", "otf") }
+
+        attach(get("ep_fonts")) { includeExtensions("ttf", "otf") }
+
+        if (propertyExists("OP")) {
+            attach(get("opfonts")) { includeExtensions("ttf", "otf") }
+        }
+
+        if (propertyExists("ED")) {
+            attach(get("edfonts")) { includeExtensions("ttf", "otf") }
+        }
+		
+		onMissingGlyphs(ErrorMode.IGNORE)
+
+        out(get("muxout"))
+    }
+
+
+}
